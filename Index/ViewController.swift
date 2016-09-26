1ed5504aeea0670450371ddf0620fce7bfa53a76
Brian Budge <brianbudge@brians-macbook.local>
2016-09-25 20:49:52 -0400

    Initial Commit

---
+++ Index/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Index
+//
+//  Created by Brian Budge on 9/25/16.
+//  Copyright © 2016 Brian Budge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
