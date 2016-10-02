f750926f2a93289aeba8b9666a88ef115e3deecf
Brian Budge <brianbudge@Brians-MacBook.local>
2016-10-02 17:08:30 -0400

    Scrolling is workinggit add .

---
+++ Index/PaperScrollView.swift
@@ -0,0 +1,26 @@
+//
+//  PaperScrollView.swift
+//  Index
+//
+//  Created by Brian Budge on 10/2/16.
+//  Copyright © 2016 Brian Budge. All rights reserved.
+//
+
+import UIKit
+
+class PaperScrollView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        panGestureRecognizer.minimumNumberOfTouches = 2;
+        panGestureRecognizer.maximumNumberOfTouches = 2;
+    }
+}
