724a80814335f171ad8a68dbb0a97ad0e0c74076
Brian Budge <brianbudge@Brians-MacBook.local>
2016-10-04 23:02:35 -0400

    Changed page size

---
+++ Index/PaperBackgroundView.swift
@@ -0,0 +1,21 @@
+//
+//  PaperBackgroundView.swift
+//  Index
+//
+//  Created by Brian Budge on 10/4/16.
+//  Copyright © 2016 Brian Budge. All rights reserved.
+//
+
+import UIKit
+
+class PaperBackgroundView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
