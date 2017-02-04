3731571e19c2f767e1fe09cb80f37444515611c8
Brian Budge <brianbudge@Brians-MacBook.local>
2017-02-04 14:51:52 -0500

    Fixed link placement bug

--- Index/Link.swift
+++ Index/Link.swift
@@ -18,13 +18,13 @@ class Link: NSObject {
     
     init(origin: CGPoint, noteTitle: String) {
         super.init()
-        self.origin = origin
         self.noteTitle = noteTitle
     
         self.button = UIButton(type: .custom)
         self.button.setTitle(self.noteTitle, for: .normal)
         self.button.setTitleColor(UIColor.blue, for: .normal)
         let buttonSize = (self.noteTitle as NSString).size(attributes: [NSFontAttributeName: self.button.titleLabel?.font ?? UIFont.systemFont(ofSize: 12)])
+        self.origin = CGPoint(x: origin.x, y: origin.y - 6)
         self.button.frame = CGRect(origin: self.origin, size: CGSize(width: buttonSize.width, height: buttonSize.height))
         self.button.addTarget(self, action: #selector(loadLink), for: .touchUpInside)
     }
